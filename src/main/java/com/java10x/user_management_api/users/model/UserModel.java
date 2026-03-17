110ed08ddcc2263a130a295a0e230d1096ed4687
H4ttiz <leonardocbizerra@gmail.com>
2026-03-17 14:25:12 -0300

    feat: Place Lombok in TaskMolde and UserModel class for abstraction

--- src/main/java/com/java10x/user_management_api/users/model/UserModel.java
+++ src/main/java/com/java10x/user_management_api/users/model/UserModel.java
@@ -3,10 +3,14 @@ package com.java10x.user_management_api.users.model;
 
 import com.java10x.user_management_api.tasks.model.entity.TaskModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserModel {
 
     @Id
@@ -24,36 +28,4 @@ public class UserModel {
     @JoinColumn(name = "id_tasks")
     private TaskModel tasks;
 
-    public UserModel() {
-    }
-
-    public UserModel(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }

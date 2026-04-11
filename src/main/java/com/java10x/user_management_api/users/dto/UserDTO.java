216ef72d03bf7102c867752d48d29dbf765a3180
H4ttiz <leonardocbizerra@gmail.com>
2026-04-10 21:04:22 -0300

    feat: Cria o mapper e DTO para entidade user

---
+++ src/main/java/com/java10x/user_management_api/users/dto/UserDTO.java
@@ -0,0 +1,4 @@
+package com.java10x.user_management_api.users.dto;
+
+public class UserDTO {
+}
---
+++ src/main/java/com/java10x/user_management_api/users/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.java10x.user_management_api.users.mapper;
+
+public class UserMapper {
+}

e8e0f34c9dd45e54740dd1bca7b9ba710704dba3
H4ttiz <leonardocbizerra@gmail.com>
2026-04-10 22:17:01 -0300

    Chore: Inclui a verção do DTO da entidade e mapeia manualmente no UserMapper os caminhos entidade x dto

--- src/main/java/com/java10x/user_management_api/users/dto/UserDTO.java
+++ src/main/java/com/java10x/user_management_api/users/dto/UserDTO.java
@@ -1,4 +1,20 @@
 package com.java10x.user_management_api.users.dto;
 
+import com.java10x.user_management_api.tasks.model.entity.TaskModel;
+import com.java10x.user_management_api.users.model.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+    private TaskModel tasks;
 }
--- src/main/java/com/java10x/user_management_api/users/mapper/UserMapper.java
+++ src/main/java/com/java10x/user_management_api/users/mapper/UserMapper.java
@@ -1,4 +1,34 @@
 package com.java10x.user_management_api.users.mapper;
 
+import com.java10x.user_management_api.users.dto.UserDTO;
+import com.java10x.user_management_api.users.model.entity.UserModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+
+    public UserModel map(UserDTO userDTO) {
+
+        UserModel userModel = new UserModel();
+        userModel.setId(userDTO.getId());
+        userModel.setName(userDTO.getName());
+        userModel.setEmail(userDTO.getEmail());
+        userModel.setPassword(userDTO.getPassword());
+        userModel.setRole(userDTO.getRole());
+        userModel.setTasks(userDTO.getTasks());
+
+        return userModel;
+    }
+
+    public UserDTO map(UserModel userModel) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(userModel.getId());
+        userDTO.setName(userModel.getName());
+        userDTO.setEmail(userModel.getEmail());
+        userDTO.setPassword(userModel.getPassword());
+        userDTO.setRole(userModel.getRole());
+        userDTO.setTasks(userModel.getTasks());
+
+        return userDTO;
+    }
 }
